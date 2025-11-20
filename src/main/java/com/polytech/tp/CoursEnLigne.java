edb91dd69c785b0c70e54e86ec835d479b96acf6
FENGHOUR charaf eddine <your-email@example.com>
2025-11-20 15:04:34 +0100

    Initial commit

---
+++ src/main/java/com/polytech/tp/CoursEnLigne.java
@@ -0,0 +1,18 @@
+package com.polytech.tp;
+
+public class CoursEnLigne extends CoursDecorator {
+    
+    public CoursEnLigne(ICours cours) {
+        super(cours);
+    }
+
+    @Override
+    public String getDescription() {
+        return coursDecorated.getDescription() + " (En ligne)";
+    }
+
+    @Override
+    public double getDuree() {
+        return coursDecorated.getDuree();
+    }
+}
---
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -0,0 +1,44 @@
+package com.polytech.tp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionnaireEmploiDuTemps implements Subject {
+    private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void attach(Observer o) {
+        if (!observers.contains(o)) {
+            observers.add(o);
+        }
+    }
+
+    @Override
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
+    public void ajouterCours(ICours cours) {
+        this.listeCours.add(cours);
+        String message = "Nouveau cours ajouté : " + cours.getDescription();
+        System.out.println(message);
+        notifyObservers(message);
+    }
+
+    public void modifierCours(ICours cours, String message) {
+        System.out.println("Cours modifié : " + message);
+        notifyObservers("Cours modifié : " + message);
+    }
+
+    public void setChangement(String message) {
+        notifyObservers(message);
+    }
+}
